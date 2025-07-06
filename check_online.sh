c444897a5fd7d153b6535e2190db12fdebf15a17
bash.girlie <94557891+politelychaotic@users.noreply.github.com>
2025-07-06 16:47:48 -0500

    Update check_online.sh
    Added datetime info to each log and added sleep at end of script to limit activity and memory usage

--- check_online.sh
+++ check_online.sh
@@ -3,16 +3,19 @@ function check_interface
     #set -o pipefail;
     /sbin/ethtool "$1" | grep -q "link detected: yes";
 }
-
+time=$(date);
 if check_interface eth0; then
-    echo "eth0: Online" > /var/log/check_online.log;
+    echo  "[${time}] eth0: Online" > /var/log/check_online.log;
 elif !check_interface; then
-    echo "eth0: Offline" > /var/log/check_online.log;
+    echo "[${time}] eth0: Offline" > /var/log/check_online.log;
 fi
 
+time=$(date);
 if check_interface wlan0; then
-    echo "wlan0: Online" > /var/log/check_online.log;
+    echo "[${time}] wlan0: Online" > /var/log/check_online.log;
 elif !check_interface wlan0; then
-    echo "wlan0: Offline" > /var/log/check_online.log;
+    echo "[${time}] wlan0: Offline" > /var/log/check_online.log;
 fi
+
+sleep 5;
     
