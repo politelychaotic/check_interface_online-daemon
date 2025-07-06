f85a36b69ec293fea40353e99985d323d3ef927f
bash.girlie <94557891+politelychaotic@users.noreply.github.com>
2025-07-06 17:14:30 -0500

    Update check_online.sh

--- check_online.sh
+++ check_online.sh
@@ -5,19 +5,20 @@ function check_interface
     set -o pipefail;
     /sbin/ethtool "$1" | grep -q "link detected: yes";
 }
-time=$(date);
-if check_interface eth0; then
-    echo  "[${time}] eth0: Online" >> /var/log/check_online.log;
-elif ! check_interface eth0; then
-    echo "[${time}] eth0: Offline" >> /var/log/check_online.log;
-fi
+while true
+do
+    time=$(date);
+    if check_interface eth0; then
+        echo  "[${time}] eth0: Online" >> /var/log/check_online.log;
+    elif ! check_interface eth0; then
+        echo "[${time}] eth0: Offline" >> /var/log/check_online.log;
+    fi
 
-time=$(date);
-if check_interface wlan0; then
-    echo "[${time}] wlan0: Online" >> /var/log/check_online.log;
-elif ! check_interface wlan0; then
-    echo "[${time}] wlan0: Offline" >> /var/log/check_online.log;
-fi
-
-sleep 5;
+    if check_interface wlan0; then
+        echo "[${time}] wlan0: Online" >> /var/log/check_online.log;
+    elif ! check_interface wlan0; then
+        echo "[${time}] wlan0: Offline" >> /var/log/check_online.log;
+    fi
+    
+    sleep 5;
     
